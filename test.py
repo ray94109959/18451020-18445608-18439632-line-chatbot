9814a65fcb01aae67113321d7e7dba15e99030b2
Ray <yhwray-git@yahoo.com>
2020-03-30 00:54:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 import redis
-
+import json
+import urllib
+import urllib.request
 
 # fill in the following.
 # HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
@@ -28,12 +30,34 @@ while True:
     count = 0
     if redis1.get(msg):
         count = int(redis1.get(msg))
-    if msg == '2':
-        print(" 2222222 " + msg, end=' ') 
+    
 
     value = 1 + count
     redis1.set(msg, value)
 
     print('for '+ str(value) +' times' )
+    
+    if msg == '2':
+        print(" 2222222 " + msg, end=' ') 
+    # a Python object (dict):
+    x = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+    }
 
+    # convert into JSON:
+    y = json.dumps(x)
+
+    # the result is a JSON string:
+    print(y)
+
+    url = 'https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Flatest_situation_of_reported_cases_wuhan_eng.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%7D' 
+   
+    data = urllib.request.urlopen(url).read().decode()
+    obj = json.loads(data)
+    last = len(obj)-1
+
+    print(obj[last])
+    print(obj[last]['Number of confirmed cases'])
 
