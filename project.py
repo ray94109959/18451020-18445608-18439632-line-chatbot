40e560b680af50865ad78b67df84fec7273d0fb3
Ray <yhwray-git@yahoo.com>
2020-03-30 15:34:06 +0800

    Update project.py

--- project.py
+++ project.py
@@ -83,6 +83,8 @@ def handle_TextMessage(event):
     msg = ''
     #msg = 'You said: "' + event.message.text + '" '
     
+    if event.message.text == '1':
+        msg = "Sorry, I'm not sure if I can help with that and still under the learning process. Your conversation with COVID-19 may be recorded for training, quality control and dispute handling purposes. Thanks!!"
     if event.message.text == '2':
         url = 'https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Flatest_situation_of_reported_cases_wuhan_eng.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%7D' 
         operUrl = urllib.request.urlopen(url)
@@ -96,6 +98,8 @@ def handle_TextMessage(event):
             msg = msg + report 
         else:
             msg = "Server is busy, please try again later....."   
+    if event.message.text == '3':
+        msg = "Sorry, I'm not sure if I can help with that and still under the learning process. Your conversation with COVID-19 may be recorded for training, quality control and dispute handling purposes. Thanks!!"
     else:    
         msg = "Sorry, I'm not sure if I can help with that and still under the learning process. Your conversation with COVID-19 may be recorded for training, quality control and dispute handling purposes. Thanks!!"
 
