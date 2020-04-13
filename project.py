0ab0af048d467b2bfeb85da9c9fe728bddaab9a2
Ray <yhwray-git@yahoo.com>
2020-04-13 12:40:29 +0800

    Update project.py

--- project.py
+++ project.py
@@ -114,7 +114,7 @@ def handle_TextMessage(event):
             msg = "Latest situation of reported cases of COVID-19 in Hong Kong:\n\n"
 
             msg = msg + str(obj[last]).replace("[","").replace("]","").replace("{","").replace("}","").replace('"',"").replace("'","").replace("\\n"," ").replace(", ","\n").replace("Number of ","") 
-            msg = msg + "\nSource: https://data.gov.hk/en-data/dataset/hk-dh-chpsebcddr-novel-infectious-agent/resource/e92c2d48-9269-4836-8e1a-5f10377f618d\n"
+            msg = msg + "\n\nSource: https://data.gov.hk/en-data/dataset/hk-dh-chpsebcddr-novel-infectious-agent/resource/e92c2d48-9269-4836-8e1a-5f10377f618d"
         else:
             msg = "Server is busy, please try again later....."   
     elif txt == '3':
