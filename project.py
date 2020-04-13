a6f70999e1f90b8daf19217b19f495a5edd51ca2
Ray <yhwray-git@yahoo.com>
2020-04-13 19:31:51 +0800

    Update project.py

--- project.py
+++ project.py
@@ -145,7 +145,7 @@ def handle_TextMessage(event):
         else:
             msg = "Server is busy, please try again later....."  
 
-    msg = msg + "\n\nCould you please tell me what are you looking for?\n1. Face Mask information\n2. Case in Hong Kong\n3. Health Tips\n\nKindly press 1, 2, 3\nOR\nInput building name/location for searching mandatory home quarantine"
+    msg = msg + "\n\nCould you please tell me what are you looking for?\n1. Face Mask information\n2. Case in Hong Kong\n3. Health Tips\n\nKindly press 1, 2, 3\nOR\nInput building name/location for searching mandatory home quarantine (eg. Central)"
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(msg)
